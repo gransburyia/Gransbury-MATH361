4fcc7ca5f0a1c3ad27ae1aba9610fc1b7bb0693e
gransburyia <igransbu@mail.umw.edu>
2019-04-26 23:47:34 -0400

    Final Commit A.3 (I swiched to lists format because I was not able to clean my polynomial of zeros)

--- Applied/A3.py
+++ Applied/A3.py
@@ -6,57 +6,76 @@ Created on Wed Apr 17 12:26:20 2019
 """
 
 import numpy as np
+#Erase high degree terms that are now zero
+# Assumed that highest degree of p is last element of list
+def clean_poly():
+    highest_deg = len(r_list) - 1   
+    for ii in range(len(r_list)-1,-1,-1):
+        if np.abs(r_list[ii]) > 1e-15:
+            break
+        else:
+            highest_deg -= 1
+    del r_list[highest_deg+1:]
 
-def ltgDividesltr(x, y):
-    if x >= y:
+def rempty(): #checks if r = 0
+    for i in r_list:
+        if i != 0:
+            return False
+    return True
+
+def ltgDividesltr(): #input will be degrees for the first terms
+    if len(r_list) >= len(g_list):
         return True
     else:
         return False
     
-def rempty():
-    if r_array[0][0] == 0 and r_array[0][0] == 0:
-        return True
+def calculatingQ():
+    #adds ltr/ltg to q
+    if len(r_list) == len(g_list):
+        q_list.insert(0, r_list[-1]/g_list[-1])
     else:
-        return False
+        #adds 0 to the list of missing place values
+        for i in range(0, len(r_list) - len(g_list) -1):
+            q_list.insert(i, 0)
+        q_list.insert(len(r_list) - len(g_list), r_list[-1]/g_list[-1])
+        
+def calculatingR():
+    #gets ltr/ltg
+    temp_r = [0]
+    if len(r_list) == len(g_list):
+        temp_r.insert(0, r_list[-1]/g_list[-1])
+    else:
+        for i in range(0, len(r_list) - len(g_list) -1):
+            temp_r.insert(i, 0)
+        temp_r.insert(len(r_list) - len(g_list), r_list[-1]/g_list[-1])
+    #multiplies ltr/ltg by g and multiplies by -1
+    temp_g = list(g_list)
+    for i in range(0,len(temp_g)):
+        temp_g[i] = (temp_g[i] * temp_r[-1]) * -1
+    for i in range(0,len(temp_r)-1):
+        temp_g.insert(0,0)
+    #adds g to r
+    for i in range(0,len(g_list)):
+        r_list[i] = r_list[i] + temp_g[i]
+    clean_poly()
+
     
-def dividingTerms(x, y): #input will be r_array[0] and g_array[0]
-    coef = x[0][0] / y[0][0]
-    power = x[0][1] - y[0][1]
-    return np.array([coef, power])
-
-def mulTermsByG(array):
-    h = np.array([0,0])
-    for i in range(0, len(g_array)):
-        power = array[0][1]
-        hpower = g_array[i][1] + power
-        coef = array[0][0]
-        hcoef = g_array[i][1] * coef
-        h = np.vstack([h, np.array([hcoef, hpower])])
-        return h
-        
-#def addingTerms(array):
-        
-#def subTerms():
-        
-def makeNegative(array):
-    for i in range(0, len(array)):
-        array[i][0] = -1 * array[i][0]
-
-
+"""
+main
+"""
             
-    
+g_list = [2, 1]
 
-g_array = np.array([[1,1],[2,0],[0,0]])
+f_list = [-1, 0, 0, 1]
 
-f_array = np.array([[1,3],[-1,0]])
+q_list = [0]
 
-q_array = np.array([0,0])
+r_list = list(f_list)
 
-r_array = f_array
+while rempty() == False and ltgDividesltr() == True:
+    calculatingQ()
+    calculatingR()
 
-while rempty() == False and ltgDividesltr == True:
-    #q += dividingTerms(r_array[0], g_array[0])
-    #r -= mulTermsByG(dividingTerms(r_array[0], g_array[0]))
-    
+
 
 
